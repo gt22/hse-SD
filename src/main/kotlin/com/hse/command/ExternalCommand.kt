df006872574bc57fd50bc33e7a0b00b21ed6727b
gt22 <igorengel@mail.ru>
2022-01-30 21:34:54 +0300

    Implement external command

--- src/main/kotlin/com/hse/command/ExternalCommand.kt
+++ src/main/kotlin/com/hse/command/ExternalCommand.kt
@@ -1,16 +1,29 @@
 package com.hse.command
 
 import com.hse.CommandContext
+import java.io.IOException
 
-class ExternalCommand(private val line: String) : AbstractCommand() {
+class ExternalCommand(private val command: String) : AbstractCommand() {
     override fun match(cmd: List<String>) = true
 
     override fun execute(arguments: List<String>, ctx: CommandContext): Int {
-        val process = Runtime.getRuntime().exec(line)
+        val cmd = ArrayList<String>(arguments.size + 1)
+        cmd[0] = command
+        cmd.addAll(arguments)
+        val process =
+            ProcessBuilder(cmd)
+                .directory(ctx.shell.workingDirectoryAbsolutePath.toFile())
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .start()
+        try {
+            ctx.input.transferTo(process.outputStream)
+        } catch (e: IOException) {
+            // process ended
+        }
         process.waitFor()
         process.inputStream.use {
-            ctx.output.write(it.readAllBytes())
+            it.transferTo(ctx.output)
         }
-        return 0 //TODO
+        return process.exitValue()
     }
 }
