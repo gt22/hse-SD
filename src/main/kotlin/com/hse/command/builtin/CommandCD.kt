2f22de305e6526cfd60c0a93f56ebe8014881069
Roman Venediktov <e2e4b6b7@gmail.com>
2022-03-05 21:13:05 +0300

    Add errors processing

--- src/main/kotlin/com/hse/command/builtin/CommandCD.kt
+++ src/main/kotlin/com/hse/command/builtin/CommandCD.kt
@@ -3,6 +3,7 @@ package com.hse.command.builtin
 import com.hse.CommandContext
 import com.hse.command.SimpleCommand
 import java.nio.file.Path
+import kotlin.io.path.isDirectory
 
 /**
  * Класс, реализующий встроенную команду изменения текущей директории.
@@ -11,7 +12,16 @@ import java.nio.file.Path
  */
 class CommandCD : SimpleCommand("cd") {
     override fun execute(arguments: List<String>, ctx: CommandContext): Int {
+        if (arguments.size > 1) {
+            ctx.writer.println("cd: too many arguments")
+            return 1
+        }
         val path = Path.of(arguments.firstOrNull() ?: System.getProperty("user.home"))
+        val newPath = ctx.shell.workingDirectoryAbsolutePath.resolve(path)
+        if (!newPath.isDirectory()) {
+            ctx.writer.println("cd: $path: No such directory")
+            return 1
+        }
         ctx.shell.workingDirectoryAbsolutePath = ctx.shell.workingDirectoryAbsolutePath.resolve(path)
         return 0
     }
